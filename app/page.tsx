671d94a3f4e08c0980204cd2a655f8637a08aad2
Italo Gama <italoflorencio@gmail.com>
2023-03-02 19:14:04 -0300

    Ajuste labels dos campos

--- app/page.tsx
+++ app/page.tsx
@@ -363,7 +363,7 @@ function Home() {
               <div className="flex flex-row -mx-3">
                 <div className="w-full px-3">
                   <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                    Data
+                    Data de Nascimento
                   </label>
                   <input
                     className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -453,7 +453,7 @@ function Home() {
               <div className="flex flex-row -mx-3">
                 <div className="w-full px-3">
                   <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                    Rua
+                    Endereço (Rua)
                   </label>
                   <input
                     className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
